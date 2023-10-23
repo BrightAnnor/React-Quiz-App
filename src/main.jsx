6658b84e6ced254c6b8a1c89c5e81f5ec0b0cf01
BrightAnnor <annor170bright@gmail.com>
2023-10-23 12:51:00 +0000

    update: setting up interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Index from '.'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <Index />
   </React.StrictMode>,
 )
