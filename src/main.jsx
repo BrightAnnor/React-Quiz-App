a176dc2b6014525cafd0466942592159a8d6787e
Jamila Umar <123380753+milat4@users.noreply.github.com>
2023-10-24 12:42:31 +0000

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Index from './index'
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './app.jsx'
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Index />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
